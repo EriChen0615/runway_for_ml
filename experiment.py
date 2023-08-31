c58b66c6f72867706209a0e001c052e45a56c9cf
Weizhe Lin <wl356@login-e-2.data.cluster>
2023-08-31 15:39:36 +0100

    [fix] fix some bugs

--- experiment.py
+++ experiment.py
@@ -415,10 +415,10 @@ class RunwayExperiment:
         
         # trainer from args
         # trainer = Trainer.from_argparse_args(args, **additional_args)
-        trainer = Trainer(**args, **additional_args)
         logger.info(f"arguments passed to trainer: {str(args)}")
         logger.info(f"additional arguments passed to trainer: {str(additional_args)}")
-        
+        trainer = Trainer(**additional_args)
+
         # trainer = pl.Trainer(**train_config.get('trainer_paras', {}), default_root_dir=self.train_dir ,callbacks=callback_list)
         trainer.fit(self.rw_executor, **train_config.get('trainer_fit_paras', {}))
     
